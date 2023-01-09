73aae0cc1c9811233cf17530faba466e9109be65
NeelParihar <neelparihar599@gmail.com>
2023-01-09 21:20:17 +0530

    added short url controller
    Signed-off-by: NeelParihar <neelparihar599@gmail.com>

---
+++ src/controllers/shorturl.controller.ts
@@ -0,0 +1,41 @@
+import { getShortUrlRepo, ShortUrl } from '../db/entities/ShortUrl.entity'
+var { customAlphabet } =  require('nanoid')
+
+const nanoid = customAlphabet('0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ', 6)
+
+export async function getShortCodeDetails(shortCode: string): Promise<ShortUrl | undefined> {
+    const savedEntity = await getShortUrlRepo().findOne(shortCode, { select: ['shortCode', 'longUrl'] })
+    return savedEntity
+}
+
+export async function createRandomShortCode(longUrl: string): Promise<ShortUrl> {
+
+  const randomShortCode = nanoid()
+
+  // if the shortcode exists
+  if (await getShortCodeDetails(randomShortCode)) {
+    return await createRandomShortCode(longUrl)
+  }
+
+  return await createSpecificShortCode(randomShortCode, longUrl)
+}
+
+export async function createSpecificShortCode(shortCode: string, longUrl: string): Promise<ShortUrl> {
+  const newEntity = new ShortUrl()
+
+  // check if shortCode is less than 4 chars in length
+  if (shortCode.length <= 4) {
+    throw new Error('ShortCode is too short')
+  }
+
+  newEntity.shortCode = shortCode
+  newEntity.longUrl = longUrl
+
+  if(await getShortCodeDetails(shortCode)){
+    throw new Error('ShortCode is already in use')
+  }
+  console.log(newEntity)
+
+  const savedEntity = await getShortUrlRepo().save(newEntity)
+  return savedEntity
+}
---
+++ src/routes/api.ts
@@ -0,0 +1,61 @@
+import { Router } from 'express'
+import { createSpecificShortCode, getShortCodeDetails, createRandomShortCode } from '../controllers/shorturl.controller'
+
+const route = Router()
+
+route.get('/:code/stats', async(req, res) => {
+  const shortCode = req.params.code
+  // validate <= 6 char
+  if (shortCode.length > 6) {
+    return res.status(400).json({
+      status: 'error',
+      message: 'Invalid short Code, too long!',
+    })
+  }
+  const savedShortCode = await getShortCodeDetails(shortCode)
+  if (!savedShortCode) {
+    return res.status(404).json({
+      status: 'error',
+      message: 'Invalid short code. Not found',
+    })
+  }
+
+  return res.status(200).json({
+    status: 'success',
+    data: savedShortCode,
+  })
+})
+
+route.post('/submit', async(req, res) => {
+  const longUrl = req.body.url
+  console.log(">>>>>>",req.body)
+  const savedShortCode = await createRandomShortCode(longUrl)
+
+  return res.status(201).json({
+    status: 'success',
+    data: savedShortCode,
+  })
+})
+
+route.put('/submit/:code', async(req, res) => {
+  const shortCode = req.params.code
+  const longUrl = req.body.url
+
+  let savedShortCode
+
+  try {
+    savedShortCode = await createSpecificShortCode(shortCode, longUrl)
+  } catch (err: any) {
+    return res.status(400).json({
+      status: 'error',
+      message: err.message,
+    })
+  }
+
+  return res.status(201).json({
+    status: 'success',
+    data: savedShortCode,
+  })
+})
+
+export default route
