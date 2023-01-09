b20f87b41ec200cbe0b3872b42e29d958796001c
NeelParihar <neelparihar599@gmail.com>
2023-01-09 20:04:03 +0530

    initialize backend server
    Signed-off-by: NeelParihar <neelparihar599@gmail.com>

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express'
+import cors from 'cors'
+// import apiRoute from './routes/api'
+// import redirectRoute from './routes/redirect'
+
+const app = express()
+
+app.use(express.json())
+app.use(cors())
+
+// app.use('/api', apiRoute)
+// app.use('/', redirectRoute)
+
+export default app
